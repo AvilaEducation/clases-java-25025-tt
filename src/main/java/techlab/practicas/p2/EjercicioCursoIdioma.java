df403efd4238718f8eb22c7ec182bb700600feeb
AvilaEducation <marcoantonio.avilatapia@bue.edu.ar>
2025-04-16 19:06:30 -0300

    after class: ejercicios practicos de la clase 2 y algunos extra

---
+++ src/main/java/techlab/practicas/p2/EjercicioCursoIdioma.java
@@ -0,0 +1,14 @@
+package techlab.practicas.p2;
+
+public class EjercicioCursoIdioma {
+    /**
+     * En un colegio existe la posibilidad de elegir, en la materia "lengua extranjera",
+     * entre las siguientes opciones: [I]nglés, [F]rancés, y [A]lemán. Se ingresa,
+     * para cada alumno, la lengua elegida. La computadora muestra el porcentaje de
+     * alumnos que eligió cada lengua
+     * DATO: pueden suponer que se van a inscribir 10 estudiantes
+     * */
+    public static void main(String[] args) {
+
+    }
+}
