dbb22ef8961ad35cc395a34a9ad68a34476236c9
AvilaEducation <marcoantonio.avilatapia@bue.edu.ar>
2025-05-07 19:06:58 -0300

    afterclass 6: POO con encapsulamiento y colaboracion entre clases

---
+++ src/main/java/techlab/practicas/p6/Cliente.java
@@ -0,0 +1,33 @@
+package techlab.practicas.p6;
+
+public class Cliente {
+    private String nombre;
+    private String email;
+
+    // Constructor
+    public Cliente() {}
+
+    public Cliente(String nombre, String email) {
+        this.nombre = nombre;
+        this.setEmail(email);
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (email.contains("@")){
+            this.email = email;
+        }
+    }
+}
