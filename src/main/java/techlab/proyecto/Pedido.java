fd81e3e039ea37672fea1a8f3922b7d0341d9ad4
AvilaEducation <marcoantonio.avilatapia@bue.edu.ar>
2025-05-21 19:27:00 -0300

    afterclass 8: proyecto pre-entrega parte 2

---
+++ src/main/java/techlab/proyecto/Pedido.java
@@ -0,0 +1,16 @@
+package techlab.proyecto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private List<Producto> productos;
+
+    public Pedido() {
+        this.productos = new ArrayList<>();
+    }
+
+    public void agregarProductoAPedido(Producto producto){
+        this.productos.add(producto);
+    }
+}
